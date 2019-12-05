283c3920b1ab6da925e678242f11c229a31cc685
First Last <salmanashraf853@gmail.com>
2019-12-05 17:25:45 +0100

    d

---
+++ build/precache-manifest.ad047898fba105beb3dfc641a08452d7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cb26bae28823ff36539c22d02851eaf",
+    "url": "/achat/index.html"
+  },
+  {
+    "revision": "ceeb9865a7e9b77679aa",
+    "url": "/achat/static/css/main.1bfa16e0.chunk.css"
+  },
+  {
+    "revision": "8d066a7c7b878d5c53cc",
+    "url": "/achat/static/js/2.5e75f468.chunk.js"
+  },
+  {
+    "revision": "ceeb9865a7e9b77679aa",
+    "url": "/achat/static/js/main.4f8301cc.chunk.js"
+  },
+  {
+    "revision": "e932d4e2896ee0522b09",
+    "url": "/achat/static/js/runtime~main.08e50bf8.js"
+  },
+  {
+    "revision": "adbccee0708ae3b7a71d9652fb353299",
+    "url": "/achat/static/media/28719144-86dc0f70-73b1-11e7-911d-60d70fcded21.adbccee0.png"
+  },
+  {
+    "revision": "5665aa816a503a7a4ebfee14a5db5edc",
+    "url": "/achat/static/media/alert.5665aa81.m4a"
+  }
+]);
